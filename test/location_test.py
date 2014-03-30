e88381ebb4bcef9d4a58feb2a56be81415851f8c
Maxim Klimin <nobus@ya.ru>
2014-03-31 00:01:03 +0400

    update location test

---
+++ test/location_test.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+
+path = "/Users/nobus/develop/multirog"
+sys.path.append(path)
+
+
+from location import Location
+
+
+class LocationTest(unittest.TestCase):
+    def setUp(self):
+        self.loc = Location()
+
+    def test_get(self):
+        l = self.loc.get()
+        self.assertTrue(l)
+
+    def test_get_yx(self):
+        x = y = self.loc.size-1
+        a = self.loc.get(y=y, x=x)
+        self.assertIsNotNone(a)
+
+
+if __name__ == '__main__':
+    unittest.main()
