5db41d40bcfa48cdece0f4bd794aa11cdeea6834
nobus <nobus@yandex-team.ru>
2014-04-11 22:53:35 +0400

    add tree in location.

--- location.py
+++ location.py
@@ -2,14 +2,22 @@ import random
 
 
 class Location:
-    def __init__(self):
-        self.size = 20
+    def __init__(self, size=20, tree_perc=15):
+        self.size = size
+        self.tree_perc = tree_perc
         self.generate()
+
+    def get_tree(self):
+        x = random.randint(0, 99)
+        if x <= self.tree_perc:
+            return 1
+        else:
+            return 0
 
     def generate(self):
         self.loc = []
         for y in xrange(0, self.size):
-            a = map(lambda x: 0, xrange(0, self.size))
+            a = map(lambda x: self.get_tree(), xrange(0, self.size))
             self.loc.append(a)
 
     def get(self, y=None, x=None):
