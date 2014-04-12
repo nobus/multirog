caf2fff21f72c93a5e345c95c505f8420f5519fa
nobus <nobus@yandex-team.ru>
2014-04-12 17:34:53 +0400

    Add world generator. Add player methods.

--- world.py
+++ world.py
@@ -1,4 +1,28 @@
+from location import Location
+from player import Player
 
 
 class World:
-    pass
+    def __init__(self, size=3):
+        self.size = size
+        self.generate()
+
+        self.start_location = self.locations[self.size / 2][self.size / 2]
+        self.players = {}
+
+    def generate(self):
+        self.locations = []
+
+        for y in xrange(0, self.size):
+            a = map(lambda x: Location(), xrange(0, self.size))
+            self.locations.append(a)
+
+    def add_player(self, name):
+        self.players[name] = Player(name, self.start_location)
+
+    def del_player(self, name):
+        if name in self.players:
+            del self.players[name]
+
+    def get_player(self, name):
+        return self.players.get(name, None)
