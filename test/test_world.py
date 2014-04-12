84cb3d5b8e504846ac2a072e86079d4e1b1e399c
nobus <nobus@yandex-team.ru>
2014-04-12 23:12:08 +0400

    Add tests for location methods.

--- test/test_world.py
+++ test/test_world.py
@@ -27,3 +27,40 @@ def test_del_player():
     w.add_player(n)
     w.del_player(n)
     assert w.get_player(n) is None
+
+
+def test_get_default_location():
+    w = World()
+    l = w.get_location()
+    assert l
+
+
+def test_get_location():
+    w = World()
+    size = w.size
+    l = w.get_location(size - 1, size - 1)
+    assert l
+
+
+def test_get_map_location():
+    w = World()
+    m = w.get_map_location()
+    assert type(m) == list
+
+
+def test_get_players_from_location():
+    w = World()
+    w.add_player("nobus")
+    w.add_player("bobus")
+    w.add_player("popus")
+
+    p = w.get_players_from_location()
+    assert len(p) == 3
+
+    w.del_player("bobus")
+    p = w.get_players_from_location()
+    assert len(p) == 2
+
+    assert len(p[0]) == 3
+    name = p[0][0]
+    assert name == "nobus" or name == "popus"
