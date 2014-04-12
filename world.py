3bf6e5862f975f241909176c57efe2cbb56a9e5a
nobus <nobus@yandex-team.ru>
2014-04-12 23:09:44 +0400

    Improved method del_player. Add location methods.

--- world.py
+++ world.py
@@ -21,8 +21,40 @@ class World:
         self.players[name] = Player(name, self.start_location)
 
     def del_player(self, name):
-        if name in self.players:
+        player = self.players.get(name, None)
+
+        if player:
+            loc = player.get_location()
+            loc.unregister_player(name)
             del self.players[name]
 
     def get_player(self, name):
         return self.players.get(name, None)
+
+    def get_location(self, x=None, y=None):
+        if x is None:
+            x = self.size / 2
+
+        if y is None:
+            y = self.size / 2
+
+        if x < self.size and y < self.size:
+            return self.locations[x][y]
+        else:
+            return None
+
+    def get_map_location(self, x=None, y=None):
+        location = self.get_location(x, y)
+        return location.get()
+
+    def get_players_from_location(self, x=None, y=None):
+        ret = []
+        location = self.get_location(x, y)
+        players = location.get_current_players()
+
+        for name in players:
+            player = self.players[name]
+            x, y, _ = player.get_position()
+            ret.append((name, x, y))
+
+        return ret
