a9b797a059b376665a8227180fba60f77685d335
nobus <nobus@yandex-team.ru>
2014-04-12 17:39:02 +0400

    Add turn methods.

--- player.py
+++ player.py
@@ -1,14 +1,29 @@
 
 
 class Player(object):
-    def __init__(self, name, x, y, loc_size):
+    def __init__(self, name, location):
         self.name = name
-        self.x = x
-        self.y = y
-        self.loc_size = loc_size
+        self.location = location
+        self.x, self.y = self.location.search_free_position()
+        self.direction = "down"
 
     def get_position(self):
-        return (self.x, self.y)
+        return (self.x, self.y, self.get_direction())
+
+    def get_direction(self):
+        return self.direction
+
+    def turn_up(self):
+        self.direction = "up"
+
+    def turn_down(self):
+        self.direction = "down"
+
+    def turn_left(self):
+        self.direction = "left"
+
+    def turn_right(self):
+        self.direction = "right"
 
     def _move(f):
         def m(self):
@@ -19,22 +34,30 @@ class Player(object):
 
     @_move
     def move_up(self):
-        if self.y > 0:
+        if self.get_direction() == "up":
             self.y -= 1
+        else:
+            self.turn_up()
 
     @_move
     def move_down(self):
-        if self.y < self.loc_size:
+        if self.get_direction() == "down":
             self.y += 1
+        else:
+            self.turn_down()
 
     @_move
     def move_left(self):
-        if self.x > 0:
+        if self.get_direction() == "left":
             self.x -= 1
+        else:
+            self.turn_left()
 
     @_move
     def move_right(self):
-        if self.x < self.loc_size:
+        if self.get_direction() == "right":
             self.x += 1
+        else:
+            self.turn_right()
 
     _move = staticmethod(_move)
