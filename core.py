e4f2a4459228535ca9463ca2c9e3398fe02fc34f
nobus <nobus@yandex-team.ru>
2014-04-21 13:30:28 +0400

    Add cjson.

--- core.py
+++ core.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import cjson
 import tornado.ioloop
 import tornado.web
 from tornado import websocket
+
+clients = []
+
+
+class EchoWebSocket(websocket.WebSocketHandler):
+    def open(self):
+        print "WebSocket opened"
+        clients.append(self)
+
+    def on_message(self, message):
+        try:
+            data = cjson.decode(message)
+            self.write_message(cjson.encode(data))
+        except Exception, e:
+            req = cjson.encode({"error": e})
+            self.write_message(req)
+
+    def on_close(self):
+        print "WebSocket closed"
+        clients.remove(self)
 
 
 class StatHandler(tornado.web.RequestHandler):
@@ -11,17 +32,6 @@ class StatHandler(tornado.web.RequestHandler):
         stat = "MultiROG - Multi Rouge Online Game\n"
         stat += "Version 0.1.0\n"
         self.write(stat)
-
-
-class EchoWebSocket(websocket.WebSocketHandler):
-    def open(self):
-        print "WebSocket opened"
-
-    def on_message(self, message):
-        self.write_message(u"You said: " + message)
-
-    def on_close(self):
-        print "WebSocket closed"
 
 
 def main():
