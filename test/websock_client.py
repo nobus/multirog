9e77ab1917382663dd8c99afb1c53605e1d13731
nobus <nobus@yandex-team.ru>
2014-04-21 13:31:05 +0400

    Add cjson.

--- test/websock_client.py
+++ test/websock_client.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import cjson
 import websocket
+
+
+def get_request(login):
+    req = {}
+    req["login"] = login
+    req["action"] = "move_up"
+
+    return cjson.encode(req)
 
 
 def main():
     url = "ws://localhost:8888/websock"
-    websocket.enableTrace(True)
+
+    #websocket.enableTrace(True)
     ws = websocket.create_connection(url)
-    ws.send("Hello, World")
+    ws.send(get_request("nobus1"))
     result = ws.recv()
+
+    try:
+        result = cjson.decode(result)
+    except Exception, e:
+        print e
+
     print "Result: ", result
     ws.close()
 
